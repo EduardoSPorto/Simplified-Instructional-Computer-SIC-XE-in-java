945b3a1ca9170b607bb5780ac39c11a4b16770d0
SandJunior <114614503+SandJunior@users.noreply.github.com>
2025-02-05 17:22:12 -0300

    Update UserInstruction.java

--- Simulador/src/simulador/instrucao/UserInstruction.java
+++ Simulador/src/simulador/instrucao/UserInstruction.java
@@ -115,14 +115,14 @@ public class UserInstruction {
 		return false;
 	}
 	public boolean isIndirect () {
-		int mask = 0b00000011;
+		int mask = 0b00000010;
 		int flags = this.instruction[0]&mask;
 		if (flags == 0b00000010)
 			return true;
 		return false;
 	}
 	public boolean isImmediate () {
-		int mask = 0b00000011;
+		int mask = 0b00000001;
 		int flags = this.instruction[0]&mask;
 		if (flags == 0b00000001)
 			return true;
